0e1f49e3b1b367e3333e3e0366f24f4791c98528
Skywodd <skywodd@gmail.com>
2013-08-07 13:26:35 +0200

    Add current envelope code (exponential response TODO)

---
+++ Envelope.cpp
@@ -0,0 +1,148 @@
+/* Dependencies */
+#include <stdint.h>
+#include "Envelope.h"
+
+namespace CheapTune {
+
+Envelope::Envelope(EnvelopeState_t state) :
+		_adsr(), _state(state), _timeTick(0), _currentTick(0) {
+}
+
+Envelope::Adsr& Envelope::adsr() {
+	return _adsr;
+}
+
+void Envelope::setState(EnvelopeState_t state) {
+
+	/* Store the new state and reset the tick counter */
+	_state = state;
+	_currentTick = 0;
+
+	/* Switch according the new state */
+	switch (state) {
+	case ENV_DISABLE:
+	case ENV_SUSTAIN:
+	case ENV_ENDED:
+		_timeTick = 0xFFFF; // Avoid the getSample() routine to do useless stuff
+		break;
+
+	case ENV_ATTACK:
+		_timeTick = _adsr.attack_time;
+		break;
+
+	case ENV_DECAY:
+		_timeTick = _adsr.decay_time;
+		break;
+
+	case ENV_RELEASE:
+		_timeTick = _adsr.release_time;
+		break;
+	}
+}
+
+uint8_t Envelope::linearResponse(uint16_t t, uint16_t T, uint8_t MIN, uint8_t MAX) {
+	return ((uint16_t) MAX * t) / T + MIN; // Simple y = ax + b equation
+}
+
+uint8_t Envelope::getSample() {
+
+	/* Check if the envelope is disable */
+	if (_state == ENV_DISABLE)
+		return 255;
+
+	/* Check if the envelope has ended */
+	if (_state == ENV_ENDED)
+		return 0;
+
+	/* Check if the current state has finish */
+	if (_currentTick == _timeTick) {
+
+		/* Reset the tick counter */
+		_currentTick = 0;
+
+		/* Switch according the current state */
+		switch (_state) {
+		case ENV_ATTACK:
+			/* Test if the decay time is null */
+			if (_adsr.decay_time) {
+
+				/* Upgrade the envelope state to the DECAY phase */
+				_state = ENV_DECAY;
+				_timeTick = _adsr.decay_time;
+
+			} else { /* No decay phase */
+
+				/* Upgrade the envelope state to the SUSTAIN phase */
+				_state = ENV_SUSTAIN;
+				_timeTick = 0xFFFF;
+			}
+			break;
+
+		case ENV_DECAY:
+			/* Upgrade the envelope state to the SUSTAIN phase */
+			_state = ENV_SUSTAIN;
+			_timeTick = 0xFFFF;
+			break;
+
+		case ENV_SUSTAIN:
+			/* Nothing to do, this phase is stable */
+			/* Only setState(ENV_RELEASE) can make the state change */
+			break;
+
+		case ENV_RELEASE:
+			/* Upgrade the envelope state to the ENDED phase */
+			_state = ENV_ENDED;
+			return 0;
+
+		default:
+			break;
+		}
+	}
+
+	/* At this step the envelope is already upgraded if necessary and not in ENV_DISABLE or ENV_ENDED state */
+
+	{
+		/* The generated sample */
+		uint8_t sample;
+
+		/* Switch according the current state */
+		switch (_state) {
+		case ENV_ATTACK:
+			sample = linearResponse(_currentTick, _timeTick, 0, 255);
+			break;
+
+		case ENV_DECAY:
+			sample = 255 - linearResponse(_currentTick, _timeTick, 0, _adsr.sustain_level);
+			break;
+
+		case ENV_SUSTAIN:
+			sample = _adsr.sustain_level;
+			break;
+
+		case ENV_RELEASE:
+			sample = _adsr.sustain_level - linearResponse(_currentTick, _timeTick, 0, _adsr.sustain_level);
+			break;
+
+		default:
+			break;
+		}
+
+		/* Increment the tick counter */
+		++_currentTick;
+
+		/* Return the generated sample */
+		return sample;
+	}
+}
+
+void Envelope::reset() {
+	_adsr.attack_time = 0;
+	_adsr.decay_time = 0;
+	_adsr.sustain_level = 0;
+	_adsr.release_time = 0;
+	_state = ENV_DISABLE;
+	_timeTick = 0;
+	_currentTick = 0;
+}
+
+}
---
+++ Envelope.h
@@ -0,0 +1,123 @@
+/**
+ * @brief Envelope generator (ADSR)
+ * @author SkyWodd
+ * @version 1.0
+ * @see http://skyduino.wordpress.com/
+ *
+ * @section licence_sec License
+ *  This program is free software: you can redistribute it and/or modify\n
+ *  it under the terms of the GNU General Public License as published by\n
+ *  the Free Software Foundation, either version 3 of the License, or\n
+ *  (at your option) any later version.\n
+ * \n
+ *  This program is distributed in the hope that it will be useful,\n
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
+ *  GNU General Public License for more details.\n
+ * \n
+ *  You should have received a copy of the GNU General Public License\n
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.\n
+ */
+#ifndef ENVELOPE_H
+#define	ENVELOPE_H
+
+/** CheapTune namespace */
+namespace CheapTune {
+
+/* Forward declaration of the Channel class */
+class Channel;
+
+/**
+ * ADSR envelope generator
+ */
+class Envelope {
+public:
+
+	/**
+	 * The ADSR data model structure
+	 */
+	struct Adsr {
+		uint16_t attack_time;  /*!< Attack time (in tick) */
+		uint16_t decay_time;   /*!< Decay time (in tick) */
+		uint8_t sustain_level; /*!< Sustain level (amplitude) */
+		uint16_t release_time; /*!< Release time (in tick) */
+	};
+
+	/**
+	 * Envelope states enumeration
+	 */
+	typedef enum {
+		ENV_DISABLE, //!< Envelope disable
+		ENV_ATTACK,  //!< Attack phase
+		ENV_DECAY,   //!< Decay phase
+		ENV_SUSTAIN, //!< Sustain phase
+		ENV_RELEASE, //!< Release phase
+		ENV_ENDED    //!< End of envelope
+	} EnvelopeState_t;
+
+protected:
+	/** ADSR envelope data */
+	Adsr _adsr;
+
+	/** Envelope state */
+	EnvelopeState_t _state;
+
+	/** Time tick before state upgrade */
+	uint16_t _timeTick;
+
+	/** Current time tick counter */
+	uint16_t _currentTick;
+
+	/**
+	 * Compute an amplitude sample from the ADSR envelope
+	 *
+	 * @return The computed amplitude sample
+	 */
+	uint8_t getSample();
+
+	/**
+	 * Simple linear response formula
+	 *
+	 * @param t Time value between 0 and T
+	 * @param T Max time value
+	 * @param MIN Min amplitude value
+	 * @param MAX Max amplitude value
+	 * @return The linear response for the given time value
+	 */
+	static uint8_t linearResponse(uint16_t t, uint16_t T, uint8_t MIN, uint8_t MAX);
+
+public:
+	/**
+	 *
+	 * @param adsr
+	 * @param state
+	 */
+	Envelope(EnvelopeState_t state = ENV_DISABLE);
+
+	/**
+	 * Get a reference to the ADSR data model of this envelope
+	 *
+	 * return A reference to the ADSR data model of this envelope
+	 */
+	Adsr& adsr();
+
+	/**
+	 * Set the state of the envelope
+	 *
+	 * @param state The new state of the envelope
+	 */
+	void setState(EnvelopeState_t state);
+
+	/**
+	 * Reset all parameters to their default values
+	 */
+	void reset();
+
+	/** Channel class can access to protected members */
+	friend Channel;
+};
+
+}
+
+#endif	/* ENVELOPE_H */
+
